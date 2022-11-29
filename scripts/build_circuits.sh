58016680ba07ac4b597f412dfcd5fae3d333b92b
semaraugusto <semaraugusto@dcc.ufmg.br>
2022-11-29 16:11:05 -0300

    test/fix issues with fresh repo

--- scripts/build_circuits.sh
+++ scripts/build_circuits.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+PTAU_PATH=./powersOfTau28_hez_final_12.ptau
+
+if [ ! -f "$PTAU_PATH" ]; then
+    curl -o $PTAU_PATH https://hermez.s3-eu-west-1.amazonaws.com/$PTAU_PATH
+fi
+
+
 compile () {
     local circuit="$1"
     mkdir -p build/$circuit
@@ -11,7 +18,6 @@ compile () {
     echo -e "Done!\n"
 }
 
-PTAU_PATH=./powersOfTau28_hez_final_22.ptau
 
 compile_phase2 () {
     local circuit="$1" pathToCircuitDir="$2"
@@ -21,14 +27,14 @@ compile_phase2 () {
     echo "Setting up Phase 2 ceremony for $circuit"
     echo "Outputting circuit_final.zkey and verifier.sol to $circuit"
 
-    npx snarkjs groth16 setup "$pathToCircuitDir/$circuit.r1cs" $PTAU_PATH "$circuit/circuit_0000.zkey"
-    echo "test" | npx snarkjs zkey contribute "$circuit/circuit_0000.zkey" "$circuit/circuit_0001.zkey" --name"1st Contributor name" -v
-    npx snarkjs zkey verify "$pathToCircuitDir/$circuit.r1cs" $PTAU_PATH "$circuit/circuit_0001.zkey"
-    npx snarkjs zkey beacon "$circuit/circuit_0001.zkey" "$circuit/circuit_final.zkey" 0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f 10 -n="Final Beacon phase2"
-    npx snarkjs zkey verify "$pathToCircuitDir/$circuit.r1cs" $PTAU_PATH "$circuit/circuit_final.zkey"
-    npx snarkjs zkey export verificationkey "$circuit/circuit_final.zkey" "$circuit/verification_key.json"  
+    npx snarkjs groth16 setup "$pathToCircuitDir/$circuit.r1cs" $PTAU_PATH "$pathToCircuitDir/circuit_0000.zkey"
+    echo "test" | npx snarkjs zkey contribute "$pathToCircuitDir/circuit_0000.zkey" "$pathToCircuitDir/circuit_0001.zkey" --name"1st Contributor name" -v
+    npx snarkjs zkey verify "$pathToCircuitDir/$circuit.r1cs" $PTAU_PATH "$pathToCircuitDir/circuit_0001.zkey"
+    npx snarkjs zkey beacon "$pathToCircuitDir/circuit_0001.zkey" "$pathToCircuitDir/circuit_final.zkey" 0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f 10 -n="Final Beacon phase2"
+    npx snarkjs zkey verify "$pathToCircuitDir/$circuit.r1cs" $PTAU_PATH "$pathToCircuitDir/circuit_final.zkey"
+    npx snarkjs zkey export verificationkey "$pathToCircuitDir/circuit_final.zkey" "$pathToCircuitDir/verification_key.json"  
 
-    npx snarkjs zkey export solidityverifier "$circuit/circuit_final.zkey" $circuit/verifier.sol
+    npx snarkjs zkey export solidityverifier "$pathToCircuitDir/circuit_final.zkey" $pathToCircuitDir/verifier.sol
     echo "Done!\n"
 }
 
--- scripts/test_inputs.sh
+++ scripts/test_inputs.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 #
-echo "generating valid witness"
-pushd artifacts/circuits/test/test_js || exit
-node generate_witness.js test.wasm ../input.json ../witness.wtns
-cd ..
-echo "generating proof"
-npx snarkjs groth16 prove circuit_final.zkey witness.wtns proof.json public.json
+rootDir=$(pwd);
+artifactsDir=$rootDir/artifacts/circuits/test;
+
+echo "generating valid witness $rootDir"
+cd $artifactsDir/test_js || exit
+echo "generating valid witness $rootDir"
+node generate_witness.js test.wasm $rootDir/input.json $rootDir/witness.wtns
+echo "generating valid witness $rootDir"
+cd $rootDir || exit
+echo "generating proof $rootDir"
+npx snarkjs groth16 prove $artifactsDir/circuit_final.zkey $rootDir/witness.wtns $rootDir/proof.json $rootDir/public.json
 echo "verifying proof"
-npx snarkjs groth16 verify verification_key.json public.json proof.json
+npx snarkjs groth16 verify $artifactsDir/verification_key.json $rootDir/public.json $rootDir/proof.json
 
 
-echo "generating invalid witness"
-cd test_js || exit
-node generate_witness.js test.wasm ../invalidInput.json ../invalidWitness.wtns
-cd ..
-echo "generating proof with invalid input"
-npx snarkjs groth16 prove circuit_final.zkey invalidWitness.wtns invalidProof.json invalidPublic.json
+echo "generating invalid witness $rootDir"
+cd $artifactsDir/test_js || exit
+node generate_witness.js test.wasm $rootDir/invalidInput.json $rootDir/invalidWitness.wtns
+echo "generating invalid witness $rootDir"
+cd $rootDir || exit
+echo "generating proof $rootDir"
+npx snarkjs groth16 prove $artifactsDir/circuit_final.zkey $rootDir/invalidWitness.wtns $rootDir/invalidProof.json $rootDir/invalidPublic.json
 echo "verifying proof"
-npx snarkjs groth16 verify verification_key.json invalidPublic.json invalidProof.json
+npx snarkjs groth16 verify $artifactsDir/verification_key.json $rootDir/invalidPublic.json $rootDir/invalidProof.json
 
-echo "generating invalid witness"
-cd test_js || exit
-node generate_witness.js test.wasm ../invalidInput2.json ../invalidWitness2.wtns
-cd ..
-echo "generating proof with invalid input"
-npx snarkjs groth16 prove circuit_final.zkey invalidWitness2.wtns invalidProof2.json invalidPublic2.json
+echo "generating invalid witness 2 $rootDir"
+cd $artifactsDir/test_js || exit
+node generate_witness.js test.wasm $rootDir/invalidInput2.json $rootDir/invalidWitness2.wtns
+echo "generating invalid witness $rootDir"
+cd $rootDir || exit
+echo "generating proof $rootDir"
+npx snarkjs groth16 prove $artifactsDir/circuit_final.zkey $rootDir/invalidWitness2.wtns $rootDir/invalidProof2.json $rootDir/invalidPublic2.json
 echo "verifying proof"
-npx snarkjs groth16 verify verification_key.json invalidPublic2.json invalidProof2.json
-
-popd || exit
+npx snarkjs groth16 verify $artifactsDir/verification_key.json $rootDir/invalidPublic2.json $rootDir/invalidProof2.json
